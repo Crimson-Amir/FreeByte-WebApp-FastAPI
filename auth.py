81728fa40b52d4f10784feac7d2935254fee9471
Crimson-Amir <ytchanneltinos@gmail.com>
2024-08-02 21:06:07 +0330

    finish-loggin-page

--- auth.py
+++ auth.py
@@ -1,13 +1,10 @@
 from datetime import datetime, timedelta
-from typing import Optional
 import jwt
-from fastapi import HTTPException, Depends
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import HTTPException
 
 SECRET_KEY = "c21022c524d7c042ce8d34a0bf798d9e06dafb748ff84725f62566172c785832ebfe1f8a9ef2b113ace974e0c5ad732e149a05"
 REFRESH_SECRET_KEY = "afshfj8o2374tp12fbrpi27ge020pskdpqwojfd983fbe9pfci0q8w7dgv09qd87g08wdq98wdb09qw"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 def create_access_token(data: dict, expires_delta: timedelta = timedelta(minutes=15)):
     to_encode = data.copy()
@@ -15,7 +12,7 @@ def create_access_token(data: dict, expires_delta: timedelta = timedelta(minutes
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm="HS256")
 
-def create_refresh_token(data: dict, expires_delta: timedelta = timedelta(days=7)):
+def create_refresh_token(data: dict, expires_delta: timedelta = timedelta(days=30)):
     to_encode = data.copy()
     expire = datetime.now() + expires_delta
     to_encode.update({"exp": expire})
