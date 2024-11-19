4682fa99b25b8573739fa5047c970675cb2ec4ae
Crimson-Amir <ytchanneltinos@gmail.com>
2024-11-19 22:39:07 +0330

    fix some issues

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:amir1383amir@localhost:2053/freebyte_app"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:amir1383amir@localhost:5432/freebyte_app"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
