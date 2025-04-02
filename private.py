8ca1504ff2e264776a4edf1b06dd66d0512d20c9
Amir <140071494+Crimson-Amir@users.noreply.github.com>
2025-04-02 16:13:27 +0330

    Update private.py

--- private.py
+++ private.py
@@ -22,3 +22,8 @@ auth = {
     'username': '6840091ecb325d4ae6c32bb7ec12a24c',
     'password': '103924d8b33449b8ccd2b0d22577b641'
 }
+
+
+SECRET_KEY = "c21022c524d7c042ce8d34a0bf798d9e06dafb748ff84725f62566172c785832ebfe1f8a9ef2b113ace974e0c5ad732e149a05"
+REFRESH_SECRET_KEY = "afshfj8o2374tp12fbrpi27ge020pskdpqwojfd983fbe9pfci0q8w7dgv09qd87g08wdq98wdb09qw"
+ALGORITHM = "HS256"
